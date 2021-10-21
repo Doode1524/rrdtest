38765cbe39e85284798f127ef5c8695c02a1327a
joey <doode1524@gmail.com>
2021-10-21 13:59:22 -0400

    pages init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Page1 from './Page1';
+import Page2 from './Page2';
+import Page3 from './Page3';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <button>P1</button>
+      <button>P2</button>
+      <button>P3</button>
     </div>
   );
 }
