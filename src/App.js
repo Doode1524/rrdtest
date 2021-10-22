8d8fcb1b58103d08dadc421947da65361670f8fb
joey <doode1524@gmail.com>
2021-10-22 15:28:30 -0400

    done vid

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import './App.css';
-import Page1 from './Page1';
-import Page2 from './Page2';
-import Page3 from './Page3';
+import "./App.css";
+import Page1 from "./Page1";
+import Page2 from "./Page2";
+import Page3 from "./Page3";
+import { Switch, Route, useHistory } from "react-router-dom";
 
 function App() {
+  const history = useHistory();
+  const hello = "Hello Youtube";
+
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <button>P1</button>
-      <button>P2</button>
-      <button>P3</button>
+      <button onClick={() => history.push("/page1")}>P1</button>
+      <button onClick={() => history.push("/page2")}>P2</button>
+      <button onClick={() => history.push("/page3")}>P3</button>
+      <Switch>
+        <Route exact path="/page1" component={Page1} />
+        <Route exact path="/page2" component={Page2} />
+        <Route
+          exact
+          path="/page3"
+          render={(props) => <Page3 {...props} hello={hello} />}
+        />
+      </Switch>
     </div>
   );
 }
--- src/Page3.js
+++ src/Page3.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const Page3 = () => {
+const Page3 = (props) => {
   return (
     <div>
       <h3>Page 3</h3>
+      <h4>{props.hello}</h4>
     </div>
   );
 };
