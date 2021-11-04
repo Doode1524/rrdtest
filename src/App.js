b8881e808fd25d387da17d52385e1e90ac88349e
joey <doode1524@gmail.com>
2021-11-04 16:06:09 -0400

    v6 refactor

--- src/App.js
+++ src/App.js
@@ -2,27 +2,28 @@ import "./App.css";
 import Page1 from "./Page1";
 import Page2 from "./Page2";
 import Page3 from "./Page3";
-import { Switch, Route, useHistory } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
 function App() {
-  const history = useHistory();
+  const navigate = useNavigate();
   const hello = "Hello Youtube";
 
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <button className="button" onClick={() => history.push("/page1")}>P1</button>
-      <button onClick={() => history.push("/page2")}>P2</button>
-      <button onClick={() => history.push("/page3")}>P3</button>
-      <Switch>
-        <Route exact path="/page1" component={Page1} />
-        <Route exact path="/page2" component={Page2} />
-        <Route
-          exact
-          path="/page3"
-          render={(props) => <Page3 {...props} hello={hello} />}
-        />
-      </Switch>
+      <button
+        className="button"
+        onClick={() => navigate("/page1", { replace: false })}
+      >
+        P1
+      </button>
+      <button onClick={() => navigate("/page2", { replace: false })}>P2</button>
+      <button onClick={() => navigate("/page3")}>P3</button>
+      <Routes>
+        <Route exact path="/page1" element={<Page1 />} />
+        <Route exact path="/page2" element={<Page2 />} />
+        <Route exact path="/page3" element={<Page3 hello={hello} />} />
+      </Routes>
     </div>
   );
 }
--- src/Page2.js
+++ src/Page2.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Page2 = () => {
+
   return (
     <div>
       <h2>Page 2</h2>
